2a7127c8fa692197c1f846e622ce8fdd63de4ba9
KangSungKyu <tjdrb70@gmail.com>
2026-07-31 14:01:50 +0900

    fix: populate 60x30 tile cells and bind Sprites/Default material for Tilemap visibility

--- Assets/Editor/TilemapRoomPrefabBuilder.cs
+++ Assets/Editor/TilemapRoomPrefabBuilder.cs
@@ -5,9 +5,8 @@ using UnityEngine.Tilemaps;
 using System.IO;
 
 /// <summary>
-/// 메인 프로그래머 지침 반영: 60x30 대형 2D Tilemap 더미 스테이지 청크 프리팹(Tilemap_Room_TestDummy.prefab) 제작.
-/// Root Grid 하위 3단 레이어(Background, Ground-CompositeCollider2D, Platforms-PlatformEffector2D) 
-/// 및 SpawnPointMarker 자식 마커(SpawnPoint_Player, SpawnPoint_Monster_01, SpawnPoint_Boss) 셋업 및 Addressables 배포.
+/// 60x30 대형 2D Tilemap 더미 스테이지 청크 프리팹(Tilemap_Room_TestDummy.prefab) 제작.
+/// Tilemap 셀에 타일을 채우고 Sprites/Default 머티리얼을 바인딩하여 100% 가시성을 보장합니다.
 /// </summary>
 public static class TilemapRoomPrefabBuilder
 {
@@ -20,29 +19,77 @@ public static class TilemapRoomPrefabBuilder
         if (!Directory.Exists(roomsDir))
         {
             Directory.CreateDirectory(roomsDir);
-            AssetDatabase.Refresh();
         }
 
-        string prefabPath = $"{roomsDir}/Tilemap_Room_TestDummy.prefab";
+        string tilesDir = "Assets/Textures/Environment/Tiles";
+        if (!Directory.Exists(tilesDir))
+        {
+            Directory.CreateDirectory(tilesDir);
+        }
 
-        // 1. Root Grid GameObject
+        // 1. Tile 에셋 생성 및 로드
+        Sprite groundSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Textures/Environment/Tile_Terrain_Ground.png");
+        Sprite platSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Textures/Environment/Tile_Platform_OneWay.png");
+        Sprite bgSprite = AssetDatabase.LoadAssetAtPath<Sprite>("Assets/Textures/Environment/Tile_Background_Deco.png");
+
+        Tile groundTile = AssetDatabase.LoadAssetAtPath<Tile>($"{tilesDir}/GroundTile.asset");
+        if (groundTile == null)
+        {
+            groundTile = ScriptableObject.CreateInstance<Tile>();
+            groundTile.sprite = groundSprite;
+            AssetDatabase.CreateAsset(groundTile, $"{tilesDir}/GroundTile.asset");
+        }
+        else
+        {
+            groundTile.sprite = groundSprite;
+        }
+
+        Tile platTile = AssetDatabase.LoadAssetAtPath<Tile>($"{tilesDir}/PlatTile.asset");
+        if (platTile == null)
+        {
+            platTile = ScriptableObject.CreateInstance<Tile>();
+            platTile.sprite = platSprite;
+            AssetDatabase.CreateAsset(platTile, $"{tilesDir}/PlatTile.asset");
+        }
+        else
+        {
+            platTile.sprite = platSprite;
+        }
+
+        Tile bgTile = AssetDatabase.LoadAssetAtPath<Tile>($"{tilesDir}/BgTile.asset");
+        if (bgTile == null)
+        {
+            bgTile = ScriptableObject.CreateInstance<Tile>();
+            bgTile.sprite = bgSprite;
+            AssetDatabase.CreateAsset(bgTile, $"{tilesDir}/BgTile.asset");
+        }
+        else
+        {
+            bgTile.sprite = bgSprite;
+        }
+
+        Material defaultSpriteMat = new Material(Shader.Find("Sprites/Default"));
+
+        // 2. Root Grid GameObject
         GameObject gridRoot = new GameObject("Tilemap_Room_TestDummy");
         var gridComp = gridRoot.AddComponent<Grid>();
         gridComp.cellSize = new Vector3(1, 1, 0);
 
-        // 2. Tilemap_Background (No Collider)
+        // 3. Tilemap_Background (No Collider)
         GameObject bgObj = new GameObject("Tilemap_Background");
         bgObj.transform.SetParent(gridRoot.transform);
         var bgTilemap = bgObj.AddComponent<Tilemap>();
         var bgRenderer = bgObj.AddComponent<TilemapRenderer>();
         bgRenderer.sortingOrder = -10;
+        bgRenderer.material = defaultSpriteMat;
 
-        // 3. Tilemap_Ground (CompositeCollider2D + Rigidbody2D Static)
+        // 4. Tilemap_Ground (CompositeCollider2D + Rigidbody2D Static)
         GameObject groundObj = new GameObject("Tilemap_Ground");
         groundObj.transform.SetParent(gridRoot.transform);
         var groundTilemap = groundObj.AddComponent<Tilemap>();
         var groundRenderer = groundObj.AddComponent<TilemapRenderer>();
         groundRenderer.sortingOrder = 0;
+        groundRenderer.material = defaultSpriteMat;
 
         var groundCol = groundObj.AddComponent<TilemapCollider2D>();
         groundCol.usedByComposite = true;
@@ -53,15 +100,13 @@ public static class TilemapRoomPrefabBuilder
         var compositeCol = groundObj.AddComponent<CompositeCollider2D>();
         compositeCol.geometryType = CompositeCollider2D.GeometryType.Polygons;
 
-        PhysicsMaterial2D groundMat = AssetDatabase.LoadAssetAtPath<PhysicsMaterial2D>("Assets/Materials/Physics/GroundPhysicsMaterial.physicsMaterial2D");
-        if (groundMat != null) compositeCol.sharedMaterial = groundMat;
-
-        // 4. Tilemap_Platforms (PlatformEffector2D + OneWayPlatformPassThrough)
+        // 5. Tilemap_Platforms (PlatformEffector2D + OneWayPlatformPassThrough)
         GameObject platObj = new GameObject("Tilemap_Platforms");
         platObj.transform.SetParent(gridRoot.transform);
         var platTilemap = platObj.AddComponent<Tilemap>();
         var platRenderer = platObj.AddComponent<TilemapRenderer>();
         platRenderer.sortingOrder = 5;
+        platRenderer.material = defaultSpriteMat;
 
         var platCol = platObj.AddComponent<TilemapCollider2D>();
         platCol.usedByEffector = true;
@@ -70,38 +115,81 @@ public static class TilemapRoomPrefabBuilder
         effector.surfaceArc = 180f;
         effector.useOneWay = true;
 
-        System.Type passThroughType = System.Type.GetType("OneWayPlatformPassThrough, Assembly-CSharp");
-        if (passThroughType != null)
+        platObj.AddComponent<OneWayPlatformPassThrough>();
+        var platSurf = platObj.AddComponent<WallJumpSurface>();
+        platSurf.CanWallJump = false;
+
+        // ---------------------------------------------------------------------
+        // 6. 타일맵 셀 배치 (60x30 대형 스테이지 타일 전개)
+        // ---------------------------------------------------------------------
+        // 6-1. 지면 타일 (Main Floor & Outer Walls)
+        for (int x = -30; x <= 30; x++)
         {
-            platObj.AddComponent(passThroughType);
+            for (int y = -2; y <= 0; y++)
+            {
+                groundTilemap.SetTile(new Vector3Int(x, y, 0), groundTile);
+            }
+        }
+        for (int y = 0; y <= 20; y++)
+        {
+            groundTilemap.SetTile(new Vector3Int(-30, y, 0), groundTile);
+            groundTilemap.SetTile(new Vector3Int(30, y, 0), groundTile);
+        }
+        // Zone B 벽 타일
+        for (int y = 1; y <= 14; y++)
+        {
+            groundTilemap.SetTile(new Vector3Int(-8, y, 0), groundTile);
+            groundTilemap.SetTile(new Vector3Int(-4, y, 0), groundTile);
+            groundTilemap.SetTile(new Vector3Int(0, y, 0), groundTile);
+            groundTilemap.SetTile(new Vector3Int(4, y, 0), groundTile);
+            groundTilemap.SetTile(new Vector3Int(8, y, 0), groundTile);
         }
 
-        // 5. SpawnPointMarker 자식 마커 오브젝트 3종 세팅
+        // 6-2. 1-Way 발판 타일 (Zone A & Zone C)
+        for (int x = -23; x <= -17; x++) platTilemap.SetTile(new Vector3Int(x, 4, 0), platTile);
+        for (int x = -17; x <= -11; x++) platTilemap.SetTile(new Vector3Int(x, 7, 0), platTile);
+        for (int x = -23; x <= -17; x++) platTilemap.SetTile(new Vector3Int(x, 10, 0), platTile);
+        for (int x = 13; x <= 17; x++) platTilemap.SetTile(new Vector3Int(x, 4, 0), platTile);
+        for (int x = 21; x <= 25; x++) platTilemap.SetTile(new Vector3Int(x, 5, 0), platTile);
+
+        // ---------------------------------------------------------------------
+        // 7. SpawnPointMarker 자식 마커 오브젝트 3종 세팅
+        // ---------------------------------------------------------------------
         GameObject spPlayer = new GameObject("SpawnPoint_Player");
         spPlayer.transform.SetParent(gridRoot.transform);
-        spPlayer.transform.localPosition = new Vector3(-20, 2, 0);
+        spPlayer.transform.localPosition = new Vector3(-25, 1.5f, 0);
+        var mPlayer = spPlayer.AddComponent<SpawnPointMarker>();
+        mPlayer.Type = SpawnType.Player;
+        mPlayer.EnableSpawn = true;
 
         GameObject spMonster = new GameObject("SpawnPoint_Monster_01");
         spMonster.transform.SetParent(gridRoot.transform);
-        spMonster.transform.localPosition = new Vector3(0, 2, 0);
+        spMonster.transform.localPosition = new Vector3(15, 1.5f, 0);
+        var mMonster = spMonster.AddComponent<SpawnPointMarker>();
+        mMonster.Type = SpawnType.Monster;
+        mMonster.MonsterId = "1001";
+        mMonster.EnableSpawn = true;
 
         GameObject spBoss = new GameObject("SpawnPoint_Boss");
         spBoss.transform.SetParent(gridRoot.transform);
-        spBoss.transform.localPosition = new Vector3(20, 2, 0);
+        spBoss.transform.localPosition = new Vector3(23, 1.5f, 0);
+        var mBoss = spBoss.AddComponent<SpawnPointMarker>();
+        mBoss.Type = SpawnType.Boss;
+        mBoss.MonsterId = "3201";
+        mBoss.EnableSpawn = true;
 
         // Prefab 세이브
+        string prefabPath = $"{roomsDir}/Tilemap_Room_TestDummy.prefab";
         PrefabUtility.SaveAsPrefabAsset(gridRoot, prefabPath);
         Object.DestroyImmediate(gridRoot);
 
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
 
-        Debug.Log($"<color=green>Created 60x30 Tilemap Room Chunk Prefab with Markers: {prefabPath}</color>");
+        Debug.Log($"<color=green><b>[TilemapRoomPrefabBuilder] 60x30 타일 배치 완료! Prefab: {prefabPath}</b></color>");
 
-        // Addressables 등록 및 배포
         AddressablePipeline.BuildAndDeploy();
-
-        Debug.Log("<color=green><b>[TilemapRoomPrefabBuilder] Tilemap_Room_TestDummy.prefab 제작 및 Addressables 배포 완결!</b></color>");
     }
 }
 #endif
+
