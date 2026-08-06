820702fb596d168b0409fa4e1db0ec21afec15b0
KangSungKyu <tjdrb70@gmail.com>
2026-08-06 12:17:20 +0900

    fix: prevent hit knockback ground penetration

--- Assets/Editor/Tests/GameplayArchitectureTests.cs
+++ Assets/Editor/Tests/GameplayArchitectureTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.IO;
+using System.Reflection;
 using System.Text;
 using UnityEngine;
 
@@ -108,6 +109,65 @@ namespace QA.Tests
                     "15/30 FPS, 15/30/45 degree slopes; grounded retained, penetration <= skin, speed deviation <= 5%");
             }
             finally { Object.DestroyImmediate(motorObj); }
+        }
+
+        [Test]
+        public void Test_CombatHitOverlap_UsesMotorKnockbackWithoutGroundPenetration()
+        {
+            var player = new GameObject("Player_HitOverlap_QA");
+            var monster = new GameObject("Monster_HitOverlap_QA");
+            var groundObject = new GameObject("Ground_HitOverlap_QA");
+            try
+            {
+                player.AddComponent<Rigidbody2D>();
+                var playerCollider = player.AddComponent<CapsuleCollider2D>();
+                playerCollider.size = new Vector2(1f, 2f);
+                var motor = player.AddComponent<KinematicMotor2D>();
+                motor.InitMotor();
+                player.transform.position = new Vector3(0f, 1f + motor.SkinWidth * 2f);
+                motor.SetGroundNormal(Vector2.up);
+                var playerStats = player.AddComponent<CombatStats>();
+                typeof(CombatStats).GetMethod("Awake", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Invoke(playerStats, null);
+
+                monster.transform.position = new Vector3(-0.25f, player.transform.position.y);
+                var monsterCollider = monster.AddComponent<CapsuleCollider2D>();
+                monsterCollider.isTrigger = true;
+                var monsterStats = monster.AddComponent<CombatStats>();
+                typeof(CombatStats).GetMethod("Awake", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Invoke(monsterStats, null);
+
+                var ground = groundObject.AddComponent<BoxCollider2D>();
+                ground.size = new Vector2(20f, 1f);
+                groundObject.transform.position = new Vector3(0f, -0.5f);
+                motor.SolidGroundLayer = 1 << groundObject.layer;
+                motor.InitMotor();
+                Physics2D.SyncTransforms();
+                Assert.IsTrue(Physics2D.Distance(playerCollider, monsterCollider).isOverlapped);
+
+                float hpBefore = playerStats.CurrentHp;
+                playerStats.TakeDamage(10f, attacker: monsterStats);
+                Assert.Less(playerStats.CurrentHp, hpBefore);
+                Assert.Greater(motor.Velocity.x, 0f);
+                Assert.GreaterOrEqual(motor.Velocity.y, 0f);
+                Assert.GreaterOrEqual(playerCollider.bounds.min.y, ground.bounds.max.y);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    motor.SimulateStep(Time.fixedDeltaTime);
+                    Physics2D.SyncTransforms();
+                    Assert.GreaterOrEqual(playerCollider.bounds.min.y, ground.bounds.max.y,
+                        $"Ground penetration after fixed step {i + 1}: bottom={playerCollider.bounds.min.y}, " +
+                        $"groundTop={ground.bounds.max.y}, body={player.transform.position}, " +
+                        $"velocity={motor.Velocity}, grounded={motor.IsGrounded}.");
+                }
+            }
+            finally
+            {
+                Object.DestroyImmediate(groundObject);
+                Object.DestroyImmediate(monster);
+                Object.DestroyImmediate(player);
+            }
         }
     }
 }
